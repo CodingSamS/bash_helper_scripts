32a36eb630e728dff64968e241cd1bb18725ede1
SamS <coding_sams@tuta.io>
2021-08-17 23:43:29 +0200

    quotes for paths

--- convert_videos/convert_videos.sh
+++ convert_videos/convert_videos.sh
@@ -43,8 +43,9 @@ then
   exit 0
 fi
 
-mkdir -p "$(dirname $log_file)"
-echo -e "Starting Video Conversion Script\n" | tee $log_file
+logfile_dirname=$(dirname "$log_file")
+mkdir -pv "$logfile_dirname"
+echo -e "Starting Video Conversion Script\n" | tee "$log_file"
 
 skipped_files=()
 conversion_failed=()
@@ -60,52 +61,55 @@ do
   current_input_line=$((current_input_line+1))
   
   # check if input file exists and is readable by the user
-  if ! [[ -r $input_name ]]
+  if ! [[ -r "$input_name" ]]
   then
-    echo "File '$input_name' does not exists or user has no permission to read it." | tee -a $log_file
-    echo "Skipping file..." | tee -a $log_file
+    echo "File $input_name does not exists or user has no permission to read it." | tee -a "$log_file"
+    echo "Skipping file..." | tee -a "$log_file"
     skipped_files+=("\n$input_name")
     continue
   fi
 
   # check if output file already exists
-  if [[ -a $output_name ]]
+  if [[ -a "$output_name" ]]
   then
-    echo "Output file '$output_name' does already exist." | tee -a $log_file
-    echo "Skipping file..." | tee -a $log_file
+    echo "Output file $output_name does already exist." | tee -a "$log_file"
+    echo "Skipping file..." | tee -a "$log_file"
     skipped_files+=("\n$input_name")
     continue
   fi
 
   # create output directory if it does not exist
-  mkdir -p "$(dirname $output_name)" | tee -a $log_file
+  output_dirname=$(dirname "$output_name")
+  mkdir -pv "$output_dirname" | tee -a "$log_file"
 
   # convert video file
-  ffmpeg -nostdin -i $input_name -c:v libx265 -map 0 -c:s dvd_subtitle -crf 20 $output_name | tee -a $log_file
+  ffmpeg -nostdin -i "$input_name" -c:v libx265 -map 0 -c:s dvd_subtitle -crf 20 "$output_name" | tee -a "$log_file"
 
   # check for the exit code of ffmpeg to determine success (=delete the source file) or failure (=delete destination)
   if [[ ${PIPESTATUS[0]} -eq 0 ]]
   then
-    echo "Conversion succeeded. Deleting source file..." | tee -a $log_file
-    rm -v $input_name | tee -a $log_file
+    echo "Conversion succeeded. Deleting source file..." | tee -a "$log_file"
+    rm -v "$input_name" | tee -a "$log_file"
     conversion_succeeded+=("\n$input_name")
   else
-    echo "Conversion failed. Deleting destination file - might be incomplete or corrupt..." | tee -a $log_file
-    rm -v $output_name | tee -a $log_file
+    echo "Conversion failed. Deleting destination file - might be incomplete or corrupt..." | tee -a "$log_file"
+    rm -v "$output_name" | tee -a "$log_file"
     conversion_failed+=("\n$input_name")
   fi
 
   # removing input and output directories (and their parents) if they are empty
-  rmdir -pv "$(dirname $output_name)" | tee -a $log_file
-  rmdir -pv "$(dirname $input_name)" | tee -a $log_file
+  output_dirname=$(dirname "output_name")
+  rmdir -pv "$output_dirname" | tee -a "$log_file"
+  input_dirname=$(dirname "input_name")
+  rmdir -pv "$input_dirname" | tee -a "$log_file"
   echo "input: $input_name"
   echo "output: $output_name"
 done < $i_o_csv
 
-echo -e "\nExecution finished." | tee -a $log_file
-echo -e "Skipped input files: ${skipped_files[*]}" | tee -a $log_file
-echo -e "Failed input files: ${conversion_failed[*]}" | tee -a $log_file
-echo -e "Successful input files: ${conversion_succeeded[*]}" | tee -a $log_file
+echo -e "\nExecution finished." | tee -a "$log_file"
+echo -e "Skipped input files: ${skipped_files[*]}" | tee -a "$log_file"
+echo -e "Failed input files: ${conversion_failed[*]}" | tee -a "$log_file"
+echo -e "Successful input files: ${conversion_succeeded[*]}" | tee -a "$log_file"
 
 
 
